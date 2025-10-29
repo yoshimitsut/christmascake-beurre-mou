8ea94c8aa6ad874593ced4b3c3e78f2bd70ebb38
yoshimitsut <shimitsutanaka@gmail.com>
2025-10-29 09:35:40 -0300

    alteracao na pagina de inf do bolo

--- frontend/src/pages/CakeInformations.tsx
+++ frontend/src/pages/CakeInformations.tsx
@@ -73,7 +73,7 @@ export default function CakeInformations() {
               style={{
                 margin: "20px auto",
                 borderCollapse: "collapse",
-                fontSize: "1.5rem"
+                fontSize: "2rem"
               }}
             >
               <tbody>
