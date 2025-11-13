5f73194c94ee090aa7a2dcc0b814eb59cfbd761f
yoshimitsut <shimitsutanaka@gmail.com>
2025-11-13 11:10:53 -0300

    mudando cor da tabela

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,6 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '1234',
-  // database: process.env.DB_NAME || 'beurre_mou_christmascake',
   database: process.env.DB_NAME || 'bmchristmascake',
   waitForConnections: true,
   connectionLimit: 10,
